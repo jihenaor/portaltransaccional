53d3fb0330884bd3ece3c62fe315451e0b435708
jorghere <jorge.henao@ceiba.com.co>
2022-03-02 14:50:19 -0500

    Diario

--- src/main/java/com/serviciudad/model/FacturaRequest.java
+++ src/main/java/com/serviciudad/model/FacturaRequest.java
@@ -1,12 +1,16 @@
 package com.serviciudad.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class FacturaRequest {
     private String codsuscrip;
+
+    public FacturaRequest(String codsuscrip) {
+        this.codsuscrip = codsuscrip;
+    }
 }
