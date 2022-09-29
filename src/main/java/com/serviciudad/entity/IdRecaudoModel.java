83f36c9418c9272f123333c5ea2aba384fb60450
jihenaor <jihenaor@gmail.com>
2022-09-29 11:49:00 -0500

    log

--- src/main/java/com/serviciudad/entity/IdRecaudoModel.java
+++ src/main/java/com/serviciudad/entity/IdRecaudoModel.java
@@ -1,9 +1,10 @@
 package com.serviciudad.entity;
 
+import com.serviciudad.compartido.model.ValueIdDomain;
 import com.serviciudad.compartido.model.ValueLongDomain;
 import com.serviciudad.compartido.model.ValueObjectDomain;
 
-public class IdRecaudoModel extends ValueObjectDomain {
+public class IdRecaudoModel extends ValueIdDomain {
     public IdRecaudoModel(String value) {
         super(value, 40);
     }
