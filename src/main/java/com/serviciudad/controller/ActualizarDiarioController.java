34838b3bc5f081affed8c99aade6ba1b3ce75bf6
jihenaor <jihenaor@gmail.com>
2022-09-29 10:21:25 -0500

    log

--- src/main/java/com/serviciudad/controller/ActualizarDiarioController.java
+++ src/main/java/com/serviciudad/controller/ActualizarDiarioController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api")
 public final class ActualizarDiarioController {
@@ -16,13 +18,17 @@ public final class ActualizarDiarioController {
     @Autowired
     private ErrorService errorService;
 
+    static final Logger LOGGER = Logger.getLogger(String.valueOf(ActualizarDiarioController.class));
+
     @RequestMapping(value = "/actualizarpendientes", method = RequestMethod.GET)
     @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<String> actualizardiario() {
 
+        LOGGER.info("Inicia actualizarpendientes");
         try {
             return ResponseEntity.ok().body(facturaService.seleccionarPagosPendientes() + "");
         } catch (Exception e) {
+            LOGGER.info("Error actualizarpendientes " + e.getMessage());
             errorService.save(e);
             return ResponseEntity.internalServerError().body(null);
         }
