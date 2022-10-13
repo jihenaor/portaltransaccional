53d2ac75ba88dca394061d5a976d2a6dd2917bf8
jorghere <jorge.henao@ceiba.com.co>
2022-10-13 16:44:23 -0500

    Correccion nombre

--- src/main/java/com/serviciudad/service/FacturaEvertecServiceInterface.java
+++ src/main/java/com/serviciudad/service/FacturaEvertecServiceInterface.java
@@ -8,7 +8,7 @@ import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface FacturaEvertecServiceInrerface {
+public interface FacturaEvertecServiceInterface {
     @Retryable(value = DomainExceptionCuentaNoExiste.class)
     FacturaResponse consultarFactura(FacturaRequest facturaRequest) throws DomainExceptionCuentaNoExiste;
 
